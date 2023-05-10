ff433bba99b69c8fa03c83e75f4dbde3f46806be
Relyna <2061746081@qq.com>
2023-05-10 17:41:20 +0800

    Change Scripts

--- Assets/Scripts/SwitchScene.cs
+++ Assets/Scripts/SwitchScene.cs
@@ -1,17 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.
 
 public class SwitchScene : MonoBehaviour
 {
-    public void SwitchToBeforeLive()
-    {
-        SceneManager.LoadScene("BeforeLive");
-    }
-    public void SwichToLive()
-    {
-        SceneManager.LoadScene("Live");
-    }
-    
+    void SwitchToBeforeLive()
 }
