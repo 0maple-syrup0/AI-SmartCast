ea920fbf1c88550d24c230bd32b950fba76f2a74
Relyna <2061746081@qq.com>
2023-05-10 17:40:52 +0800

    Add Scene Switch

--- Assets/Scripts/SwitchScene.cs
+++ Assets/Scripts/SwitchScene.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SwitchScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SwitchToBeforeLive()
     {
-        
+        SceneManager.LoadScene("BeforeLive");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SwichToLive()
     {
-        
+        SceneManager.LoadScene("Live");
     }
+    
 }
